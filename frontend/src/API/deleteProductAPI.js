abc435ea39b80cb5fb33d618bd7d4fbc5efb62e2
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2021-05-28 15:25:52 -0300

    a

---
+++ frontend/src/API/deleteProductAPI.js
@@ -0,0 +1,45 @@
+async function deleteProductAPI(productsAPI,token,id){
+
+     const headers = new Headers();
+        headers.append('Accept', 'application/json');
+     headers.append('Authorization', `Bearer ${token}`);
+
+
+        const setting = {
+          method: 'DELETE',
+          headers: headers,
+
+        }
+
+const deleteConfirmation = window.confirm('El producto será eliminado de la base de datos ¿Esta seguro? ');
+
+if(deleteConfirmation){
+
+try {
+      
+
+  
+  
+  let res = await fetch(`/api/products/${id}`, setting);
+
+
+        if(res.status  ===204 ) return await productsAPI()
+  
+        if(res.status === 403) return   alert('Se require rol de Administrador') 
+
+      } catch (err) {
+
+        console.log(err)
+      }
+
+}else{
+  return
+}
+  
+
+  }
+
+  
+
+
+export default deleteProductAPI
