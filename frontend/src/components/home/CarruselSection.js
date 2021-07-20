620d9bf4941ce38c5827ff46bc661118dcc24763
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2021-07-20 12:55:21 -0300

    styling carrucel

--- frontend/src/components/home/CarruselSection.js
+++ frontend/src/components/home/CarruselSection.js
@@ -1,22 +1,20 @@
-import styled  from 'styled-components'
-import NewestProductsCarrusel from './NewestProductsCarrusel'
-import {SectionTitle} from '../menu/Menu'
+import styled from "styled-components";
+import NewestProductsCarrusel from "./NewestProductsCarrusel";
+import { SectionTitle } from "../menu/Menu";
 const Container = styled.section`
-
-width:100%;
-max-width:1240px;
-margin:0 auto;
-margin-bottom:40px;
-padding: 0 15px;
-`
- export default function CarrucelSection(){
-   return(
-     <>
-
-   <SectionTitle>Últimas Novedades</SectionTitle>
-   <Container>
-<NewestProductsCarrusel/>
-</Container>
-     </>
-   )
- }
+  width: 100%;
+  max-width: 1240px;
+  margin: 0 auto;
+  margin-bottom: 20px;
+  padding: 0 15px;
+`;
+export default function CarrucelSection() {
+  return (
+    <>
+      <SectionTitle>Últimas Novedades</SectionTitle>
+      <Container>
+        <NewestProductsCarrusel />
+      </Container>
+    </>
+  );
+}
--- frontend/src/components/home/NewestProductsCarrusel.js
+++ frontend/src/components/home/NewestProductsCarrusel.js
@@ -2,66 +2,64 @@ import React from "react";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import Item  from '../menu/MenuItem'
-import AddToCartButton from '../AddToCartButton'
-import useNewestProducts from '../../hooks/useNewestProducts'
-import ProductsCarruselSkeleton from '../ProductsCarruselSkeleton'
+import Item from "../menu/MenuItem";
+import AddToCartButton from "../AddToCartButton";
+import useNewestProducts from "../../hooks/useNewestProducts";
+import ProductsCarruselSkeleton from "../ProductsCarruselSkeleton";
 // Import Swiper styles
 import "swiper/swiper.min.css";
-import "swiper/components/pagination/pagination.min.css"
-import "swiper/components/navigation/navigation.min.css"
-
-
+import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/navigation/navigation.min.css";
 
 // import Swiper core and required modules
-import SwiperCore, {
-  Pagination,Navigation
-} from 'swiper/core';
+import SwiperCore, { Pagination, Navigation } from "swiper/core";
 
 // install Swiper modules
-SwiperCore.use([Pagination,Navigation]);
-
+SwiperCore.use([Pagination, Navigation]);
 
 export default function ProductsCarrucel() {
+  const { isLoading, latestProducts } = useNewestProducts();
 
-const {isLoading,latestProducts} = useNewestProducts()
-
-
-if(isLoading) return (<ProductsCarruselSkeleton/>)
+  if (isLoading) return <ProductsCarruselSkeleton />;
   return (
     <>
-    <Swiper slidesPerView={1} spaceBetween={20} slidesPerGroup={3} loop={true} loopFillGroupWithBlank={true} pagination={{
-  "clickable": true
-}} navigation={true} className="mySwiper"
-breakpoints={{
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={20}
+        slidesPerGroup={3}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        className="mySwiper  "
+        breakpoints={{
+          330: {
+            width: 660,
+            slidesPerView: 2,
+          },
 
-    330: {
-      width: 660,
-      slidesPerView:2 ,
-    },
-
-    660: {
-      width: 970,
-      slidesPerView: 3,
-    },
-     970: {
-      width: 1200,
-      slidesPerView: 4,
-    },
-
-  }}
->
-
-     {latestProducts.map( product => {
-
- return<SwiperSlide key={product._id} ><Item item={product}>
-                  <AddToCartButton thisProductInfo={product}/>
-
- </Item></SwiperSlide>
-    }
- )}
-
-  </Swiper>
+          660: {
+            width: 970,
+            slidesPerView: 3,
+          },
+          970: {
+            width: 1200,
+            slidesPerView: 4,
+          },
+        }}
+      >
+        {latestProducts.map((product) => {
+          return (
+            <SwiperSlide key={product._id}>
+              <Item item={product}>
+                <AddToCartButton thisProductInfo={product} />
+              </Item>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </>
-  )
+  );
 }
